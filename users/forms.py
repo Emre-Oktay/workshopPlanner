5832dae0b258db658653d67bb497b713f2485e4a
Emre-Oktay <emreoktaybuldu@gmail.com>
2024-01-12 14:38:34 +0300

    Add user choices (particiant, creator)

--- users/forms.py
+++ users/forms.py
@@ -5,20 +5,32 @@ from .models import User
 
 
 class RegistrationForm(UserCreationForm):
+    USER_TYPE_CHOICES = [
+        ('participant', 'Participant'),
+        ('creator', 'Creator'),
+    ]
+
+    user_type = forms.ChoiceField(
+        choices=USER_TYPE_CHOICES,
+        widget=forms.RadioSelect,
+        initial='participant',
+    )
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name',]
+        fields = ['username', 'email', 'first_name', 'last_name', 'user_type']
 
         widgets = {
             'username': forms.TextInput(attrs={'class': 'form-control'}),
             'email': forms.EmailInput(attrs={'class': 'form-control'}),
             'first_name': forms.TextInput(attrs={'class': 'form-control'}),
             'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control'}),
+            'password2': forms.PasswordInput(attrs={'class': 'form-control'}),
         }
 
         def __init__(self, *args, **kwargs):
             super(RegistrationForm, self).__init__(*args, **kwargs)
-
             self.fields['password1'].widget.attrs['class'] = 'form-control'
             self.fields['password2'].widget.attrs['class'] = 'form-control'
 
--- users/models.py
+++ users/models.py
@@ -3,6 +3,17 @@ from django.db import models
 
 
 class User(AbstractUser):
+    USER_TYPE_CHOICES = [
+        ('participant', 'Participant'),
+        ('creator', 'Creator'),
+    ]
+
+    user_type = models.CharField(
+        max_length=20,
+        choices=USER_TYPE_CHOICES,
+        default='participant',
+    )
+
     bio = models.TextField(blank=True)
     birth_date = models.DateField(null=True, blank=True)
     profile_picture = models.ImageField(
